08a54dfaadfb29baff6106ad78bb8db348ab1612
Gesang Paudra J <g.paudra@gmail.com>
2024-06-16 21:19:49 +0700

    add git link

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,4 +1,5 @@
-
+import Link from "next/link"
+import { GoGitPullRequest } from "react-icons/go"
 
 const Footer = () =>{
     const date = new Date()
@@ -9,6 +10,9 @@ const Footer = () =>{
         <div className="min-w-full bg-[#001d52] flex items-center w-full justify-between min-h-[80px] pl-[52px] pr-[52px]">
             <div className="text-white font-bold text-md">
                 &copy;{tahun} GESANG TECHNOLOGY
+            </div>
+            <div>
+               <Link className="text-white" href='https://github.com/GesangPJ/gtacheatdb'><GoGitPullRequest /></Link>
             </div>
         </div>
 
